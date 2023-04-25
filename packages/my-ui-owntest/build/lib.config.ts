b7af0c1943105b41168232e8b3af05a1212b7554
getTpk <l_baochuan@163.com>
2023-04-25 19:24:15 +0800

    优化

--- packages/my-ui-owntest/build/lib.config.ts
+++ packages/my-ui-owntest/build/lib.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, '../packages/index.ts'),
       name: 'MYUi',
-      fileName: (format) => `my-ui.${format}.js`
+      fileName: (format) => `my-ui-owntest.${format}.js`
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
